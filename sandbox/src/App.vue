3e4d5c76bbc23f10e534e1682ac882a14c67fe97
Colby Hearn <colbybhearn@gmail.com>
2020-12-08 13:27:47 -0600

    working example of child component using vuex store reactively

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -3,10 +3,90 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      </br>
+      <div @click="directToggleP1">direct {{cp1}}</div></br>
+      <div @click="commitToggleP1">commit {{cp1}}</div></br>
+      <div @click="dispatchToggleP1">dispatch {{cp1}}</div></br>
+      <div @click="resetOb">reset {{cob}}</div></br>
+      <VisibilityToggle :value="cp1" text='toggle1' />
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { mapState } from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    ob:{
+			p1:true,
+			p2:false,
+    },    
+  },
+  mutations: {
+    toggleP1(state){
+      state.ob.p1 = ! state.ob.p1;
+    }    
+  },
+  actions: {
+    actToggleP1(context){      
+      context.commit('toggleP1');
+    }
+  }
+})
+
+import VisibilityToggle from './visibilityToggle.vue'
+
+export default {
+  name: 'App',
+  store,  
+  data: function(){
+    return {
+     
+    }
+  },
+  computed:mapState({    
+    cp1: state => state.ob.p1,
+    cob: state => state.ob    
+  }),  
+  components: {    
+  },
+  watch:{
+    
+  },
+  created: function(){
+  },
+  mounted: function(){      
+  },
+  methods: {
+    directToggleP1(){
+      this.$store.state.ob.p1=!this.$store.state.ob.p1;
+    },
+    commitToggleP1(){
+      this.$store.commit('toggleP1');
+    } ,   
+    dispatchToggleP1(){
+      this.$store.dispatch('actToggleP1');      
+    },
+    resetOb(){
+      this.$store.state.ob={
+        p1:true,
+        p2:false
+      }
+    }
+  },
+  components:{
+    VisibilityToggle
+  }
+
+}
+</script>
 
 <style>
 #app {
---
+++ sandbox/src/visibilityToggle.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class='viewOpt' v-on:click="toggle">
+        visToggle
+        <div v-if="cVis" style='padding:0 1px 0 1px;'>{{cVis}}</div>
+        <div v-if="!cVis">{{cVis}}</div>
+        <label style='display:inline;padding-left: 5px'>{{text}}</label>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['value', 'text'],
+  data: function(){
+      return {
+          visible: this.value
+      };
+  },
+  computed:{
+      cVis(){
+          return this.$store.state.ob['p1'];
+      }
+  },
+  methods: {
+      toggle: function(){
+          this.visible=!this.visible;
+
+          //this.$emit('toggle');
+          //this.$emit('input', this.visible)
+          this.$store.dispatch("actToggleP1");
+      }
+  }
+}
+</script> 
+
+<style scoped>
+
+
+
+</style>
+
+
